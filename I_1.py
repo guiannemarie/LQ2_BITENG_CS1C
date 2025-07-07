1c5b01c7f49bf7a3f0e17316a774793f9ae88bed
guiannemarie <guiannemarie9@gmail.com>
2025-07-07 22:25:45 +0800

    I_1.py

---
+++ I_1.py
@@ -0,0 +1,4 @@
+# a. Convert Conditional to Ternary
+(age >= 18 && genAverage >= 83) 
+  ? console.log("You may enter College, under BSCS Program")
+  : console.log("Both conditions may have not met and not allowed to enroll");
